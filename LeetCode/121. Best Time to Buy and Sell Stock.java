8e1d89a28f299948fffcdb1f4ee733c4cfdabd82
MaksNazarenko <nazarenkomaks256@gmail.com>
2020-09-18 13:01:14 +0300

    add One path solution to 121. Best Time to Buy and Sell Stock.java

--- LeetCode/121. Best Time to Buy and Sell Stock.java
+++ LeetCode/121. Best Time to Buy and Sell Stock.java
@@ -1,4 +1,6 @@
 //1. brute force
+// O(n^2) time
+// O(1) space
 class Solution {
     public int maxProfit(int[] prices) {
         int ans = 0;
@@ -14,5 +16,23 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+//2. one path
+// O(n) time
+// O(1) space
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            } else if(prices[i] - minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
     }
 }
