186cfc70fd0acae4a9ac171fbc8accf7b0b57606
Maks Nazarenko <71183975+MaksNazarenko@users.noreply.github.com>
2021-02-23 00:13:42 +0200

    add solution and upd README

---
+++ hackerrank/java/com/maksnazarenko/language_proficiency/java/java_string_tokens/Solution.java
@@ -0,0 +1,25 @@
+package com.maksnazarenko.language_proficiency.java.java_string_tokens;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.trim();
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        String[] tokens = s.split("[ !,?._'\"@]+");
+        System.out.println(tokens.length);
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+        scan.close();
+    }
+}
+
