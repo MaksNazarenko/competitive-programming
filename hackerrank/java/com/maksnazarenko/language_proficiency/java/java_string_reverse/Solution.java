33749e1847a991e6ff8b6eb3a3d83c55bab05a08
Maks Nazarenko <71183975+MaksNazarenko@users.noreply.github.com>
2021-02-22 23:43:31 +0200

    add solution and upd README

---
+++ hackerrank/java/com/maksnazarenko/language_proficiency/java/java_string_reverse/Solution.java
@@ -0,0 +1,21 @@
+package com.maksnazarenko.language_proficiency.java.java_string_reverse;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        for (int i = 0; i < A.length()/2; i++) {
+            if (A.charAt(i) != A.charAt(A.length()-i-1)) {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
