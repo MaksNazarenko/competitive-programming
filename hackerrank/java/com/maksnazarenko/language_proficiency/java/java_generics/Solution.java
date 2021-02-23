94d9793df37648ff99b41749583e7d80b8473417
Maks Nazarenko <71183975+MaksNazarenko@users.noreply.github.com>
2021-02-23 22:47:19 +0200

    add solution and upd README

---
+++ hackerrank/java/com/maksnazarenko/language_proficiency/java/java_generics/Solution.java
@@ -0,0 +1,36 @@
+package com.maksnazarenko.language_proficiency.java.java_generics;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer {
+    //Write your code here
+    public <Type> void printArray(Type[] arrayOfType) {
+        for (Type item : arrayOfType) {
+            System.out.println(item);
+        }
+    }
+}
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if (name.equals("printArray"))
+                count++;
+        }
+
+        if (count > 1) System.out.println("Method overloading is not allowed!");
+
+    }
+}
