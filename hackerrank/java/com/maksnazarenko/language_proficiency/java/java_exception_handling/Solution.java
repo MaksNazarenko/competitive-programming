ccaaaccc84a960d7457bdc20e395594e2b5314c6
Maks Nazarenko <71183975+MaksNazarenko@users.noreply.github.com>
2021-02-25 11:15:55 +0200

    add solution and upd README

---
+++ hackerrank/java/com/maksnazarenko/language_proficiency/java/java_exception_handling/Solution.java
@@ -0,0 +1,44 @@
+package com.maksnazarenko.language_proficiency.java.java_exception_handling;
+
+import java.util.Scanner;
+
+class MyCalculator {
+    /*
+     * Create the method long power(int, int) here.
+     */
+    long power(int n, int p) throws Exception {
+        long ans = n;
+
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        } else if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        } else if (p == 0) {
+            ans = 1;
+        } else {
+            for (int i = 1; i < p; i++) {
+                ans *= n;
+            }
+        }
+        return ans;
+    }
+
+}
+
+public class Solution {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            int p = in.nextInt();
+
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
